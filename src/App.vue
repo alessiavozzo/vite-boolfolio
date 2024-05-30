cbfa56533dbd414471889d25cbf7c157590af26b
alessiavozzo <a.vozzo95@gmail.com>
2024-05-30 15:50:31 +0200

    api call + print data in console

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: [],
+      base_api_url: 'http://127.0.0.1:8000',
+      base_posts_url: '/api/projects',
+    }
+  },
+
+  methods: {
+    call_API(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => console.log(error));
+    }
+  },
+
+  mounted() {
+    let url = this.base_api_url + this.base_posts_url;
+    this.call_API(url);
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
