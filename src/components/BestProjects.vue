6aa23e8bd6390f474cf6934ab5a48139397aae46
alessiavozzo <a.vozzo95@gmail.com>
2024-06-04 00:23:11 +0200

    add getFavourites call to get favourite projects

---
+++ src/components/BestProjects.vue
@@ -0,0 +1,39 @@
+<script>
+import axios from "axios";
+import { state } from "../state.js";
+export default {
+    name: 'BestProjects',
+    data() {
+        return {
+            state: state,
+            favourites: [],
+        }
+    },
+    methods: {
+        getFavourites(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.fav_projects.data);
+                    this.favourites = response.data.fav_projects.data;
+                    console.log(this.favourites);
+                })
+                .catch(error => console.log(error));
+        }
+    },
+    mounted() {
+        let url = state.base_api_url + state.fav_projects_url;
+        this.getFavourites(url);
+    }
+}
+</script>
+
+<template>
+    <div>
+        {{ this.favourites }}
+    </div>
+</template>
+
+
+
+<style scoped></style>
--- src/state.js
+++ src/state.js
@@ -3,5 +3,6 @@ import { reactive } from "vue";
 export const state = reactive({
     base_api_url: 'http://127.0.0.1:8000',
     base_projects_url: '/api/projects',
+    fav_projects_url: '/api/favourites'
 })
 
